5346099e49c75189696c9d7a2c67526fe29963f4
Marthijn Verhoeven <mmamhver@student.avans.nl>
2015-02-09 14:09:00 +0100

    about toegevoegd

---
+++ About.php
@@ -0,0 +1,33 @@
+<html>
+	<header>
+	<link rel="stylesheet" content="text/css" href="CSS/BasicStyle.css">
+		<title>
+			Auto website
+		</title>
+	</header>
+	
+	<body>
+	<?php include_once "Header.php" ?>
+		
+		<div style="margin-left: 200px"=>
+			
+			<table>
+			
+				<tr>
+					<td width="600">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</td>
+				</tr>
+				
+			</table>
+			
+			<br>
+			
+			Contact: test@test.com
+			
+			
+		</div>
+
+	<?php include_once "Footer.php" ?>
+	</body>
+</html>
