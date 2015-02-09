797560d90c5abf2155952c8f34d0f2c02c013cd6
Marthijn Verhoeven <mmamhver@student.avans.nl>
2015-02-09 13:56:48 +0100

    index bewerkt

---
+++ Index.php
@@ -0,0 +1,116 @@
+<html>
+	<header>
+	<link rel="stylesheet" content="text/css" href="CSS/BasicStyle.css">
+		<title>
+			Auto website
+		</title>
+	</header>
+	
+	<body>
+	<?php include_once "Header.php" ?>
+		
+		<table style="margin-left: 200px">
+			<tr>
+				<td>
+					<table>
+						<tr>
+							<td width="150">
+								<img src="Images/camaro.png">
+							</td>
+						
+							<td>
+								<table>
+									<tr>
+										<td>
+											Camaro
+										</td>
+									</tr>
+								
+									<tr>
+										<td>
+											Speed: snel <br>
+											Sound: vroom
+										</td>
+									</tr>
+								</table>
+							
+							</td>
+						</tr>
+					</table>
+				
+					<table>
+						<tr>
+							<td width="150">
+								<img src="Images/chrysler.png" width="100" height="100">
+							</td>
+				
+							<td width="100">
+								Chrysler
+							</td>
+						
+							<td width="150">
+								Speed: zeer snel <br>
+								Sound: heng
+							</td>
+						</tr>
+					
+						<tr>
+							<td width="150">
+								<img src="Images/ferrari.png" width="100" height="100">
+							</td>
+						
+							<td width="100">
+								Ferrari
+							</td>
+						
+							<td>
+								Speed: snelst <br>
+								Sound: broem
+							</td>
+						</tr>
+					</table>
+				
+				</div>
+			
+			</div>
+		</td>
+				
+		<td valign="top">
+			<table style="margin-top: 30px">
+				<tr>
+					<td>
+						<b>NewsFeed</b>
+					</td>
+				</tr>
+						
+				<tr>
+					<td>
+						Nieuwe camaro uitgebracht!
+					</td>
+				</tr>
+						
+				<tr>
+					<td>
+						Ferrari zeer snel!
+					</td>
+				</tr>
+							
+				<tr>
+					<td>
+						lorum
+					</td>
+				</tr>
+						
+				<tr>
+					<td>
+						ipsum
+					</td>
+				</tr>
+			</table>
+		</td>
+		</tr>
+		</table>
+
+	<?php include_once "Footer.php" ?>
+	</body>
+</html>
