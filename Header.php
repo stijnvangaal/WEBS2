73bcb01973eb50ffbbb84e5b792b9902b72ef4c3
Stijn van Gaal <stijn_50@msn.com>
2015-02-09 14:13:41 +0100

    clickable menu

--- Header.php
+++ Header.php
@@ -10,9 +10,9 @@
 		</div>
 		
 		<div id="TopMenuMenuBar">
-			<a href="#" class="TopMenuMenuLink" id="TopMenuMenuLinkFirst"><div class="TopMenuMenuItem">Home</div></a>
-			<a href="#" class="TopMenuMenuLink"><div class="TopMenuMenuItem">About</div></a>
-			<a href="#" class="TopMenuMenuLink"><div class="TopMenuMenuItem">Products</div></a>
+			<a href="index.php" class="TopMenuMenuLink" id="TopMenuMenuLinkFirst"><div class="TopMenuMenuItem">Home</div></a>
+			<a href="about.php" class="TopMenuMenuLink"><div class="TopMenuMenuItem">About</div></a>
+			<a href="product.php" class="TopMenuMenuLink"><div class="TopMenuMenuItem">Products</div></a>
 		</div>
 	</div>
 <!-- menu bar -->
