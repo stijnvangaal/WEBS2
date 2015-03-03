bd15ad9a55a210f893636183a394112b2b6cfda2
Marthijn Verhoeven <mmamhver@student.avans.nl>
2015-03-03 16:14:32 +0100

    menu wordt uit database opgehaald

---
+++ Admin.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+	<link rel="stylesheet" content="text/css" href="CSS/BasicStyle.css">
+		<title>
+			Auto website
+		</title>
+	</head>
+	
+	<body>
+	<?php include_once "Header.php" ?>
+		
+	<?php include_once "Footer.php" ?>
+	</body>
+</html>
--- Header.php
+++ Header.php
@@ -11,9 +11,59 @@
 		</div>
 		
 		<div id="TopMenuMenuBar">
+			<?php
+				include "config.inc.php";
+				$link = mysqli_connect("$DB_LOCATION", "$DB_USERNAME", "$DB_PASSWORD")
+				or die ("Can't connect to MySQL Server!");
+				$db = mysqli_select_db($link, "webs2autodb") or die("Kan database niet selecteren!");
+				
+				$query = "SELECT * FROM menu";
+				
+				$isFirst = true;
+				
+				if ($result = mysqli_query($link, $query))
+				{
+					while($row = mysqli_fetch_assoc($result))
+					{
+						$url = $row["url"];
+						$name = $row["name"];
+						
+						if($isFirst)
+						{
+							echo "<a href='$url' class='TopMenuMenuLink' id='TopMenuMenuLinkFirst'><div class='TopMenuMenuItem'>$name</div></a>";
+							$isFirst = false;
+						}
+						else
+						{
+							echo "<a href='$url' class='TopMenuMenuLink'><div class='TopMenuMenuItem'>$name</div></a>";
+						}
+						
+					}
+				}
+				
+				
+			?>
+		</div>
+		<!--
+		
+		<div id="TopMenuMenuBar">
 			<a href="index.php" class="TopMenuMenuLink" id="TopMenuMenuLinkFirst"><div class="TopMenuMenuItem">Home</div></a>
 			<a href="about.php" class="TopMenuMenuLink"><div class="TopMenuMenuItem">About</div></a>
 			<a href="product.php" class="TopMenuMenuLink"><div class="TopMenuMenuItem">Products</div></a>
 		</div>
+		
+		-->
+		
+		<!--
+				foreach($list as &$menuItem)
+				{
+					$url = $menuItem["url"];
+					$name = $menuItem["name"];
+					
+					echo "<a href='$url' class='TopMenuMenuLink' id='TopMenuMenuLinkFirst'><div class='TopMenuMenuItem'>$name</div></a>";
+				}
+				-->
 	</div>
+	
+		
 <!-- menu bar -->
