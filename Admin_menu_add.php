1f256533d2eef081ae6a81ec3db1c214b65e33b3
Marthijn Verhoeven <mmamhver@student.avans.nl>
2015-03-04 15:32:26 +0100

    Admin menu pagina's aangepast

--- Admin_menu_add.php
+++ Admin_menu_add.php
@@ -1,16 +1,9 @@
-<html>
-	<head>
-	<link rel="stylesheet" content="text/css" href="CSS/BasicStyle.css">
-		<title>
-			Auto website
-		</title>
-	</head>
-	
-	<?php
-		if($_POST){
+<?php
+	if($_POST)
+	{
 		//include database connection
 		include "config.inc.php";
-		
+	
 		try{
 	 
 			//prepare query for excecution
@@ -22,58 +15,55 @@
 			$p = $_POST['Priority'];
 			$n = $_POST['Name'];
 			$u = $_POST['Url'];
-			
-			
-			
+
 			$query = "INSERT INTO menu SET Priority = $p, Name = '$n', Url = '$u'";
 			
 			// Execute the query
-			if(mysqli_query($link, $query)){
+			if(mysqli_query($link, $query))
+			{
 				echo "Record was saved.";
-			}else{
+			}
+			else
+			{
 				die('Unable to save record.' . $query );
 			}
-  
 		}
 		catch(PDOException $exception)
 		{ //to handle error
 			echo "Error: " . $exception->getMessage();
 		}
-}
-	?>
+	}
+?>
 	
+<?php include_once "Header.php" ?>
 	
-	<body>
-	<?php include_once "Header.php" ?>
+	<div id="ContentContainer">
 		
-		<div id="ContentContainer">
-			
-			<form action='#' method='post' border='0'>
-				<table>
-					<tr>
-						<td>Priority</td>
-						<td><input type='text' name='Priority' required /></td>
-					</tr>
-					<tr>
-						<td>Name</td>
-						<td><input type='text' name='Name' required /></td>
-					</tr>
-					<tr>
-						<td>URL</td>
-						<td><input type='text' name='Url' required /></td>
-					</tr>
-						<td></td>
-						<td>
-							<input type='submit' value='Save' />
-			  
-							<a href='Admin_menu.php'>Back</a>
-						</td>
-					</tr>
-				</table>
-			</form>
-
-		</div>
+		<h1>Add new menu item</h1>
 		
-	<?php include_once "Footer.php" ?>
-	</body>
-</html>
+		<form action='#' method='post' border='0'>
+			<table>
+				<tr>
+					<td>Priority</td>
+					<td><input type='text' name='Priority' required /></td>
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td><input type='text' name='Name' required /></td>
+				</tr>
+				<tr>
+					<td>URL</td>
+					<td><input type='text' name='Url' required /></td>
+				</tr>
+					<td></td>
+					<td>
+						<input type='submit' value='Save' />
+		  
+						<a href='Admin_menu.php'>Back</a>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	
+<?php include_once "Footer.php" ?>
--- Admin_menu_delete.php
+++ Admin_menu_delete.php
@@ -1,48 +1,35 @@
-<html>
-	<head>
-	<link rel="stylesheet" content="text/css" href="CSS/BasicStyle.css">
-		<title>
-			Auto website
-		</title>
-	</head>
+<?php include_once "Header.php" ?>
 	
-	<body>
-	<?php include_once "Header.php" ?>
-		
-		<?php
-			if($_POST){
-				//include database connection
-				include "config.inc.php";
+	<?php
+		if($_POST)
+		{
+			include "config.inc.php";
+			
+			try
+			{
+				$link = mysqli_connect("$DB_LOCATION", "$DB_USERNAME", "$DB_PASSWORD")
+				or die ("Can't connect to MySQL Server!");
+				$db = mysqli_select_db($link, "webs2autodb") or die("Kan database niet selecteren!");
 				
-				try{
-			 
-					//prepare query for excecution
-					
-					$link = mysqli_connect("$DB_LOCATION", "$DB_USERNAME", "$DB_PASSWORD")
-					or die ("Can't connect to MySQL Server!");
-					$db = mysqli_select_db($link, "webs2autodb") or die("Kan database niet selecteren!");
-					
-					$ID = $_POST['ID'];
-					
-					$query = "DELETE FROM menu WHERE ID=$ID";
-					
-					// Execute the query
-					if(mysqli_query($link, $query)){
-						echo "Record was deleted.";
-						header("Location: Admin_menu.php");
-					}else{
-						die('Unable to delete record.' . $query );
-					}
+				$ID = $_POST['ID'];
+				
+				$query = "DELETE FROM menu WHERE ID=$ID";
+				
+				if(mysqli_query($link, $query))
+				{
+					echo "Record was deleted.";
+					header("Location: Admin_menu.php");
 				}
-				catch(PDOException $exception)
-				{ //to handle error
-					echo "Error: " . $exception->getMessage();
+				else
+				{
+					die('Unable to delete record.' . $query );
 				}
-				
-				
 			}
-		?>
+			catch(PDOException $exception)
+			{
+				echo "Error: " . $exception->getMessage();
+			}	
+		}
+	?>
 		
 	<?php include_once "Footer.php" ?>
-	</body>
-</html>
